7ce8d2b8e7dc635d624035098b7711c0a1e00692
Yuhan Zhou <flyzyh@hotmail.com>
2018-04-24 20:07:29 +0800

    add hibernate support

--- src/main/java/com/zyh/springbtest/test1/controller/TestController.java
+++ src/main/java/com/zyh/springbtest/test1/controller/TestController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @RequestMapping("/sayHello")
-    public String test(){
+    @RequestMapping("/sayHi")
+    public String sayHi(){
         Gson gson = new Gson();
         NodeTest nodeTest = new NodeTest("syp","18");
         gson.toJson(nodeTest);
         return gson.toString();
+    }
+
+    @RequestMapping("/sayHello")
+    public String sayHello(){
+        return "hello";
     }
 }
